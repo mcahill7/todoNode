4c832f543c77d68e311c7c31592bd9fd1cd64c3d
mcahill7 <mason.j.cahill@gmail.com>
2017-06-26 12:18:36 -0400

    Modify deploy

--- awsdeploy.sh
+++ awsdeploy.sh
@@ -4,7 +4,7 @@ SERVICE_NAME="nodetodo-service"
 NEW_DOCKER_IMAGE="mcahill7/todoapp:latest"
 CLUSTER_NAME="Staging-Mason"
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
-NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
+NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[1].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
 aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME
