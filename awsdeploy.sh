31d5544bf2eacf8ed11a8aaf55de4aebc8f17402
mcahill7 <mason.j.cahill@gmail.com>
2017-06-27 10:52:58 -0400

    deployment script

--- awsdeploy.sh
+++ awsdeploy.sh
@@ -9,6 +9,6 @@ FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes:
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
 aws ecs update-service --service $SERVICE_NAME --desired-count 0 --cluster $CLUSTER_NAME
 aws ecs delete-service --service $SERVICE_NAME --cluster $CLUSTER_NAME
-sleep 500
-aws ecs create-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME --desired-count 2
+sleep 45 
+aws ecs create-service --service $SERVICE_NAME --task-definition $NEW_TASK_DEF --cluster $CLUSTER_NAME --desired-count 2
 #aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME --desired-count 2
