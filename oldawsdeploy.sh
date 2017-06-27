19d18aff866b355a55f85a0ed8a9a7f1a26f63fe
mcahill7 <mason.j.cahill@gmail.com>
2017-06-27 11:50:42 -0400

    deploy test

--- oldawsdeploy.sh
+++ oldawsdeploy.sh
@@ -7,4 +7,7 @@ OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY -
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[1].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
-aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME
+#aws ecs update-service --service $SERVICE_NAME --desired-count 0 --cluster $CLUSTER_NAME --task-definition $TASK_FAMILY
+#aws ecs delete-service --service $SERVICE_NAME --cluster $CLUSTER_NAME
+#sleep 45 
+aws ecs create-service --service $SERVICE_NAME --task-definition $TASK_FAMILY:18 --cluster $CLUSTER_NAME --desired-count 2
