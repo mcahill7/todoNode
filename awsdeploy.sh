86981d7382feee143e48732a801227b2cc926680
mcahill7 <mason.j.cahill@gmail.com>
2017-06-27 08:58:38 -0400

    Modified deployment script

--- awsdeploy.sh
+++ awsdeploy.sh
@@ -2,7 +2,7 @@
 TASK_FAMILY="ecscompose-personal"
 SERVICE_NAME="nodetodo-service"
 NEW_DOCKER_IMAGE="mcahill7/todoapp:$BUILD_NUMBER"
-CLUSTER_NAME="Staging-Mason2"
+CLUSTER_NAME="Staging-Mason"
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[1].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
