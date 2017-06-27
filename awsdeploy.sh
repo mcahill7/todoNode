d96f6eba7e28c511145ce6c397fa41dfe99f71fa
mcahill7 <mason.j.cahill@gmail.com>
2017-06-27 11:38:20 -0400

    deployment script

--- awsdeploy.sh
+++ awsdeploy.sh
@@ -10,4 +10,4 @@ aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo 
 #aws ecs update-service --service $SERVICE_NAME --desired-count 0 --cluster $CLUSTER_NAME --task-definition $TASK_FAMILY
 #aws ecs delete-service --service $SERVICE_NAME --cluster $CLUSTER_NAME
 #sleep 45 
-aws ecs create-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME --desired-count 2
+aws ecs create-service --service $SERVICE_NAME --task-definition $TASK_FAMILY:18 --cluster $CLUSTER_NAME --desired-count 2
